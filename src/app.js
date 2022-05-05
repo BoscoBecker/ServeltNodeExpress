5eaa7cb53dafc1dd4df4f286043f81111f9cf497
João bosco <joao.jbosco@gmail.com>
2022-05-05 16:50:30 -0300

    Things about security

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+
 const app = express();
 const index = require('./routes/index');
 const productRoute = require('./routes/product.routes');
 const propleRoute = require('./routes/people.routes');
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
+
 app.use(cors());
+app.use(helmet());
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.disable('x-powered-by');
+app.set('trust proxy', '127.0.0.1');
 
 app.use(index);
 app.use('/api/', productRoute);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
     res.status(200).send({
-        sucess: 'true',
-        message: 'Hello World',
-        version: '1.0.0'            
+            sucess: 'true',
+            message: 'Your are logged',
+            version: '1.0.0'
     });
 
 });
-
 
 module.exports = router;
